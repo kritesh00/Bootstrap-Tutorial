ff1a856f83805a3c764da8d0323049100e46fea1
kritesh00 <kriteshbhattarai617@gmail.com>
2026-02-01 22:35:19 +0545

    Update

--- react+bootstrap/src/Components/Nav.jsx
+++ react+bootstrap/src/Components/Nav.jsx
@@ -3,7 +3,7 @@ const Nav = ()=>{
         <>
         <nav className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
             <div className="container-fluid">
-    <a className="navbar-brand" href="#">Bootstrap Tutorial</a>
+    <a className="navbar-brand" href="#home">Bootstrap Tutorial| By kritesh</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
